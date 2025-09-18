89d985aa35a22bb4b7445c92263600af5fb9d156
jelena <jelenajermolajeva@gmail.com>
2025-09-18 11:02:21 +0300

    initial commit

---
+++ src/main/java/ee/valiit/tuulestviidudback/TuulestviidudApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.tuulestviidudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuulestviidudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuulestviidudApplication.class, args);
+	}
+
+}
