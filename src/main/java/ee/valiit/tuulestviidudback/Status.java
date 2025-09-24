13dd1aac2233dead4362870ff4d1cc090644472e
jelena <jelenajermolajeva@gmail.com>
2025-09-24 09:37:14 +0300

    admin add

---
+++ src/main/java/ee/valiit/tuulestviidudback/Status.java
@@ -0,0 +1,15 @@
+package ee.valiit.tuulestviidudback;
+
+import lombok.Getter;
+
+@Getter
+public enum Status {
+    ACTIVE("A"),
+    DELETED("D");
+
+    private final String code;
+
+    Status(String code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/ee/valiit/tuulestviidudback/SurfStatus.java
@@ -0,0 +1,4 @@
+package ee.valiit.tuulestviidudback;
+
+public enum SurfStatus {
+}
