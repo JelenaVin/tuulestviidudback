cf4cc016377fb7714d7779e0869511e18d1eb6d3
Janno <janno@janno.ee>
2025-09-29 12:15:41 +0300

    Cron job complete

--- src/main/java/ee/valiit/tuulestviidudback/TuulestviidudApplication.java
+++ src/main/java/ee/valiit/tuulestviidudback/TuulestviidudApplication.java
@@ -2,8 +2,10 @@ package ee.valiit.tuulestviidudback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TuulestviidudApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class TuulestviidudApplication {
 	}
 
 }
+
